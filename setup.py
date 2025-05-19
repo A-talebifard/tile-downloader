fd66db7ad69c46018b005a0771e4dd6477b10a79
A-talebifard <79259668+A-talebifard@users.noreply.github.com>
2025-05-19 15:35:00 +0330

    Change smth

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tile_downloader',
-    version='0.1',
+    version='0.15',
     packages=find_packages(),
     install_requires=[
         'requests',
